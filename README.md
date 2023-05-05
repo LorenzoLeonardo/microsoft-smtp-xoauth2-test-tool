d40482dba2714da741e33988d7516b36855daa79
Lorenzo Leonardo <enzotechcomputersolutions@gmail.com>
2023-05-05 20:51:59 +0800

    Improve args indexing

--- README.md
+++ README.md
@@ -4,20 +4,18 @@ This is a test tool for SMTP XOAUTH2 email workflow. This is using the device co
 
 How to use this tool
 
-cargo run \<access token grant type\> \<client secret\> \<client id\> \<sender email address\> \<sender name\> \<recipient email\> \<recipient name\> \<debug log level\>
+cargo run \<access token grant type\> \<client id\> \<client secret\> \<sender email address\> \<sender name\> \<recipient email\> \<recipient name\> \<debug log level\>
 
 
 Notes:
-
-The \<client secret\> can be of the following:
-- None (If there is no client secret)
-- Client Secret string (If there is a client secret)
-
 
 The \<access token grant type\> can be of the following:
 - AuthorizationCodeGrant
 - DeviceCodeFlow
 
+The \<client secret\> can be of the following:
+- None (If there is no client secret)
+- Client Secret string (If there is a client secret)
 
 The \<debug log level\> can be of the following:
 - error
--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,17 @@ use crate::device_code_flow::device_code_flow;
 use error::OAuth2Result;
 use token_keeper::TokenKeeper;
 
+enum ParamIndex {
+    TokenGrantType = 1,
+    ClientId,
+    ClientSecret,
+    SenderEmail,
+    SenderName,
+    RecipientEmail,
+    RecipientName,
+    DebugLevel,
+}
+
 #[derive(EnumString)]
 enum OAuth2TokenGrantFlow {
     AuthorizationCodeGrant,
@@ -39,25 +50,28 @@ fn init_logger(level: &str) {
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> OAuth2Result<()> {
     let args: Vec<String> = env::args().collect();
-    let client_secret = match args[2].as_str() {
+    let client_secret = match args[ParamIndex::ClientSecret as usize].as_str() {
         "None" => None,
-        _ => Some(ClientSecret::new(args[2].to_string())),
+        _ => Some(ClientSecret::new(
+            args[ParamIndex::ClientSecret as usize].to_string(),
+        )),
     };
-    let client_id = &args[3];
-    let sender_email = &args[4];
-    let sender_name = &args[5];
-    let receiver_email = &args[6];
-    let receiver_name = &args[7];
-    init_logger(args[8].as_str());
+    let client_id = &args[ParamIndex::ClientId as usize];
+    let sender_email = &args[ParamIndex::SenderEmail as usize];
+    let sender_name = &args[ParamIndex::SenderName as usize];
+    let receiver_email = &args[ParamIndex::RecipientEmail as usize];
+    let receiver_name = &args[ParamIndex::RecipientName as usize];
+    init_logger(args[ParamIndex::DebugLevel as usize].as_str());
 
-    let access_token = match OAuth2TokenGrantFlow::from(args[1].to_string()) {
-        OAuth2TokenGrantFlow::AuthorizationCodeGrant => {
-            auth_code_grant(client_id, client_secret, sender_email).await?
-        }
-        OAuth2TokenGrantFlow::DeviceCodeFlow => {
-            device_code_flow(client_id, client_secret, sender_email).await?
-        }
-    };
+    let access_token =
+        match OAuth2TokenGrantFlow::from(args[ParamIndex::TokenGrantType as usize].to_string()) {
+            OAuth2TokenGrantFlow::AuthorizationCodeGrant => {
+                auth_code_grant(client_id, client_secret, sender_email).await?
+            }
+            OAuth2TokenGrantFlow::DeviceCodeFlow => {
+                device_code_flow(client_id, client_secret, sender_email).await?
+            }
+        };
 
     // Start of sending Email
     let message = MessageBuilder::new()
